ebaabfc1b47b10849a7f048251080db73ea8600e
kevice <kevice@mail.com>
2025-09-19 17:06:30 +0800

    delete module kudos-ability-distributed-notify-rdb

--- kudos-ability/kudos-ability-distributed/kudos-ability-distributed-notify/kudos-ability-distributed-notify-mq/test-src/io/kudos/ability/distributed/notify/mq/main/ms/NotifyMqMsService.kt
+++ kudos-ability/kudos-ability-distributed/kudos-ability-distributed-notify/kudos-ability-distributed-notify-mq/test-src/io/kudos/ability/distributed/notify/mq/main/ms/NotifyMqMsService.kt
@@ -3,33 +3,31 @@ package io.kudos.ability.distributed.notify.mq.main.ms
 import io.kudos.ability.distributed.notify.common.api.INotifyProducer
 import io.kudos.ability.distributed.notify.common.model.NotifyMessageVo
 import io.kudos.ability.distributed.notify.mq.common.NotifyTypeEnum
-import org.soul.base.log.Log
-import org.soul.base.log.LogFactory
-import org.soul.context.core.CommonContext
-import org.soul.context.core.ContextParam
+import io.kudos.base.logger.LogFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Service
 
 @Service
 open class NotifyMqMsService {
-    private val log: Log = LogFactory.getLog(NotifyMqMsService::class.java)
+
+    private val log = LogFactory.getLog(this)
 
     @Autowired
     private val notifyProducer: INotifyProducer? = null
-    private val data: MutableMap<String?, HashSet<Int?>?> = HashMap<String?, HashSet<Int?>?>()
+    private val data: MutableMap<String?, HashSet<Int?>?> = HashMap()
 
-    private val registryMap: MutableMap<Int?, String?> = HashMap<Int?, String?>()
+    private val registryMap: MutableMap<Int?, String?> = HashMap()
 
-    @Bean
-    fun contextParam(): ContextParam {
-        val context = ContextParam()
-        // 这个数据类型目前是Integer类型, 像配置文件定义的db1,db2这种字符串会有问题
-        // jdbc模块也有相同问题, 后续建议统一调整为Object类型,并自动对String,Integer,Long等常用类型进行转换
-        context.username = "notifyMqTestUser"
-        CommonContext.set(context)
-        return context
-    }
+//    @Bean
+//    fun contextParam(): ContextParam {
+//        val context = ContextParam()
+//        // 这个数据类型目前是Integer类型, 像配置文件定义的db1,db2这种字符串会有问题
+//        // jdbc模块也有相同问题, 后续建议统一调整为Object类型,并自动对String,Integer,Long等常用类型进行转换
+//        context.username = "notifyMqTestUser"
+//        CommonContext.set(context)
+//        return context
+//    }
 
     fun process(key: String): Boolean {
         val messageVo = NotifyMessageVo<String>()
@@ -41,19 +39,19 @@ open class NotifyMqMsService {
     fun collection(port: Int?, appKey: String, key: String?): Boolean {
         var result = false
         if (registryMap.containsKey(port)) {
-            if (appKey == registryMap.get(port)) {
+            if (appKey == registryMap[port]) {
                 if (!data.containsKey(key)) {
                     data.put(key, HashSet<Int?>())
                 }
-                if (!data.get(key)!!.contains(port)) {
-                    data.get(key)!!.add(port)
+                if (!data[key]!!.contains(port)) {
+                    data[key]!!.add(port)
                     log.info("@@@ collection, port:{0}, appKey:{1}, key:{2}", port, appKey, key)
                     result = true
                 }
             } else log.info(
                 "@@@ application appkey not eq, port:{0}, validKey:{1}, appKey:{2}",
                 port,
-                registryMap.get(port),
+                registryMap[port],
                 appKey
             )
         } else {
@@ -66,7 +64,7 @@ open class NotifyMqMsService {
     fun isSync(key: String?): Boolean {
         var result = false
         if (data.containsKey(key)) {
-            if (data.get(key)!!.size == registryMap.size) result = true
+            if (data[key]!!.size == registryMap.size) result = true
         }
         return result
     }
