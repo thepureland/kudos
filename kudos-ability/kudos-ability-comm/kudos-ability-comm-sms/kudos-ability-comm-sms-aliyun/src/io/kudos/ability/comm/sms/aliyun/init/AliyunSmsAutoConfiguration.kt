4bf366191053c591853cf594cd81c30a791238c6
Kevice <kevice@mail.com>
2025-09-20 00:24:35 +0800

    rewrite AwsSmsTest

--- kudos-ability/kudos-ability-comm/kudos-ability-comm-sms/kudos-ability-comm-sms-aliyun/src/io/kudos/ability/comm/sms/aliyun/init/AliyunSmsAutoConfiguration.kt
+++ kudos-ability/kudos-ability-comm/kudos-ability-comm-sms/kudos-ability-comm-sms-aliyun/src/io/kudos/ability/comm/sms/aliyun/init/AliyunSmsAutoConfiguration.kt
@@ -1,12 +1,14 @@
 package io.kudos.ability.comm.sms.aliyun.init
 
 import io.kudos.ability.comm.sms.aliyun.handler.AliyunSmsHandler
+import io.kudos.context.config.YamlPropertySourceFactory
 import io.kudos.context.init.ContextAutoConfiguration
 import io.kudos.context.init.IComponentInitializer
 import org.springframework.beans.factory.annotation.Configurable
 import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.PropertySource
 
 
 /**
@@ -17,6 +19,10 @@ import org.springframework.context.annotation.Bean
  */
 @Configurable
 @AutoConfigureAfter(ContextAutoConfiguration::class)
+@PropertySource(
+    value = ["classpath:kudos-ability-comm-sms-aliyun.yml"],
+    factory = YamlPropertySourceFactory::class
+)
 open class AliyunSmsAutoConfiguration : IComponentInitializer {
 
     @Bean
--- kudos-ability/kudos-ability-comm/kudos-ability-comm-sms/kudos-ability-comm-sms-aws/build.gradle.kts
+++ kudos-ability/kudos-ability-comm/kudos-ability-comm-sms/kudos-ability-comm-sms-aws/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     api(project(":kudos-ability:kudos-ability-comm:kudos-ability-comm-common"))
-    api("software.amazon.awssdk:sns:2.17.230")
-    api("software.amazon.awssdk:apache-client:2.17.230")
+    api("software.amazon.awssdk:sns:2.33.13")
+    api("software.amazon.awssdk:apache-client:2.33.13")
 
-    testImplementation(project(":kudos-test:kudos-test-common"))
+    testImplementation(project(":kudos-test:kudos-test-container"))
 }
