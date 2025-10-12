07bd6d2ff9f9cbb2d84542300726ae01443e5ba9
kevice <kevice@mail.com>
2025-10-12 22:59:20 +0800

    add junit test for web servers

--- kudos-ability/kudos-ability-web/kudos-ability-web-springmvc/build.gradle.kts
+++ kudos-ability/kudos-ability-web/kudos-ability-web-springmvc/build.gradle.kts
@@ -7,4 +7,6 @@ dependencies {
     api(libs.spring.session.data.redis)
 
     testImplementation(project(":kudos-test:kudos-test-common"))
+    testImplementation(libs.spring.boot.starter.jetty)
+    testImplementation(libs.spring.boot.starter.undertow)
 }
---
+++ kudos-ability/kudos-ability-web/kudos-ability-web-springmvc/src/io/kudos/ability/web/springmvc/support/enums/ServletServerEnum.kt
@@ -0,0 +1,13 @@
+package io.kudos.ability.web.springmvc.support.enums
+
+/**
+ * springboot所支持的内嵌servlet容器枚举
+ *
+ * @author K
+ * @since 1.0.0
+ */
+enum class ServletServerEnum {
+    TOMCAT,
+    JETTY,
+    UNDERTOW
+}
