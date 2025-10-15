ea5055141161d2502c8a2bc0f4c0442975c75583
kevice <kevice@mail.com>
2025-10-15 10:55:07 +0900

    replace ktor by springmvc for ams

--- kudos-tools/resources/templates/kudos/${project}-ams-${module}/${project}-ams-${module}-api-service/build.gradle.kts
+++ kudos-tools/resources/templates/kudos/${project}-ams-${module}/${project}-ams-${module}-api-service/build.gradle.kts
@@ -3,23 +3,16 @@ dependencies {
     api(project(":kudos-ability:kudos-ability-cache:kudos-ability-cache-interservice:kudos-ability-cache-interservice-provider"))
     api(project(":kudos-ability:kudos-ability-distributed:kudos-ability-distributed-discovery:kudos-ability-distributed-discovery-nacos"))
     api(project(":kudos-ability:kudos-ability-distributed:kudos-ability-distributed-config:kudos-ability-distributed-config-nacos"))
-    api(project(":kudos-ability:kudos-ability-web:kudos-ability-web-ktor"))
+    api(project(":kudos-ability:kudos-ability-web:kudos-ability-web-springmvc"))
     <#else>api(project(":${project}-ams-${module}:${project}-ams-${module}-service"))
     api("io.kudos:kudos-ability-cache-interservice-provider")
     api("io.kudos:kudos-ability-distributed-discovery-nacos")
     api("io.kudos:kudos-ability-distributed-config-nacos")
-    api("io.kudos:kudos-ability-web-ktor")
+    api("io.kudos:kudos-ability-web-springmvc")
     </#if>
 
     //region your codes 1
 
-    // h2
-    // h2可以用PostgreSqlDialect来实现分页
-    testImplementation(libs.h2database.h2)
-
-    // postgres
-    testImplementation(libs.postgresql)
-    testImplementation(libs.ktorm.support.postgresql)
 
     //endregion your codes 1
 
