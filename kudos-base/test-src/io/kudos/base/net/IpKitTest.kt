a487f5a9050cc0b76862b935e93514445548215a
kevice <kevice@mail.com>
2025-10-21 00:51:37 +0800

    optimize IpKit

--- kudos-base/test-src/io/kudos/base/net/IpKitTest.kt
+++ kudos-base/test-src/io/kudos/base/net/IpKitTest.kt
@@ -1,5 +1,6 @@
 package io.kudos.base.net
 
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
@@ -105,14 +106,6 @@ internal class IpKitTest {
     }
 
     @Test
-    fun ipv4ToIpv6() {
-        assertEquals("0000:0000:0000:0000:0000:0000:C0A8:0001", IpKit.ipv4ToIpv6("192.168.0.1"))
-        assertEquals("0000:0000:0000:0000:0000:0000:C0A8:0001", IpKit.ipv4ToIpv6("192.168.0.001"))
-        assertEquals("999.168.0.001", IpKit.ipv4ToIpv6("999.168.0.001"))
-        assertEquals("", IpKit.ipv4ToIpv6(""))
-    }
-
-    @Test
     fun isValidIpv6() {
         assertFalse(IpKit.isValidIpv6("192.168.0.1"))
         assertFalse(IpKit.isValidIpv6("sdfdsflksdkj"))
@@ -153,20 +146,22 @@ internal class IpKitTest {
     }
 
     @Test
-    fun standardizeIpv6() {
-        assertEquals("0000:0000:0000:0000:0000:0000:C0A8:0001", IpKit.standardizeIpv6("192.168.0.1"))
+    fun toFullIpv6() {
+        assertEquals("0000:0000:0000:0000:0000:FFFF:C0A8:0001", IpKit.toFullIpv6("192.168.0.1"))
+        assertEquals("0000:0000:0000:0000:0000:FFFF:C0A8:0001", IpKit.toFullIpv6("192.168.0.001"))
+        assertThrows<IllegalArgumentException> { IpKit.toFullIpv6("999.168.0.001") }
         assertEquals(
-            "ABCD:EF01:2345:6789:ABCD:EF01:2345:6789", IpKit.standardizeIpv6("ABCD:EF01:2345:6789:ABCD:EF01:2345:6789")
+            "ABCD:EF01:2345:6789:ABCD:EF01:2345:6789", IpKit.toFullIpv6("ABCD:EF01:2345:6789:ABCD:EF01:2345:6789")
         )
-        assertEquals("2001:0DB8:0000:0023:0008:0800:200C:417A", IpKit.standardizeIpv6("2001:DB8:0:23:8:800:200C:417A"))
-        assertEquals("FF01:0000:0000:0000:0000:0000:0000:1101", IpKit.standardizeIpv6("FF01:0:0:0:0:0:0:1101"))
-        assertEquals("FF01:0000:0000:0000:0000:0000:0000:1101", IpKit.standardizeIpv6("FF01::1101"))
-        assertEquals("0000:0000:0000:0000:0000:0000:0000:0001", IpKit.standardizeIpv6("0:0:0:0:0:0:0:1"))
-        assertEquals("0000:0000:0000:0000:0000:0000:0000:0001", IpKit.standardizeIpv6("::1"))
-        assertEquals("0000:0000:0000:0000:0000:0000:0000:0000", IpKit.standardizeIpv6("0:0:0:0:0:0:0:0"))
-        assertEquals("0000:0000:0000:0000:0000:0000:0000:0000", IpKit.standardizeIpv6("::"))
-        assertEquals("0000:0000:0000:0000:0000:0000:C0A8:0001", IpKit.standardizeIpv6("::192.168.0.1"))
-        assertEquals("0000:0000:0000:0000:0000:FFFF:C0A8:0001", IpKit.standardizeIpv6("::FFFF:192.168.0.1"))
+        assertEquals("2001:0DB8:0000:0023:0008:0800:200C:417A", IpKit.toFullIpv6("2001:DB8:0:23:8:800:200C:417A"))
+        assertEquals("FF01:0000:0000:0000:0000:0000:0000:1101", IpKit.toFullIpv6("FF01:0:0:0:0:0:0:1101"))
+        assertEquals("FF01:0000:0000:0000:0000:0000:0000:1101", IpKit.toFullIpv6("FF01::1101"))
+        assertEquals("0000:0000:0000:0000:0000:0000:0000:0001", IpKit.toFullIpv6("0:0:0:0:0:0:0:1"))
+        assertEquals("0000:0000:0000:0000:0000:0000:0000:0001", IpKit.toFullIpv6("::1"))
+        assertEquals("0000:0000:0000:0000:0000:0000:0000:0000", IpKit.toFullIpv6("0:0:0:0:0:0:0:0"))
+        assertEquals("0000:0000:0000:0000:0000:0000:0000:0000", IpKit.toFullIpv6("::"))
+        assertEquals("0000:0000:0000:0000:0000:0000:C0A8:0001", IpKit.toFullIpv6("::192.168.0.1"))
+        assertEquals("0000:0000:0000:0000:0000:FFFF:C0A8:0001", IpKit.toFullIpv6("::FFFF:192.168.0.1"))
     }
 
 
