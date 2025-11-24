b9c18c9e78e5e39760ad490a3096fa5667b227e7
kevice <kevice@mail.com>
2025-11-24 21:11:29 +0800

    migrate jdk to 25

--- buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 kotlin {
     // Use a specific Java version to make it easier to work in different environments.
-    jvmToolchain(21)
+    jvmToolchain(25)
 }
 
 tasks.withType<Test>().configureEach {
