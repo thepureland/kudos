6a5de0f43551b996a2085ef4d03bfbaa156f1a3e
will <will.don@icloud.com>
2025-12-27 12:00:44 +0900

    upgrade org.gradle.toolchains.foojay-resolver-convention to 1.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ dependencyResolutionManagement {
 
 plugins {
     // Use the Foojay Toolchains plugin to automatically download JDKs required by subprojects.
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 
