1c4dd878dbd29bbe44ef64ddbf93cd16ce360da3
Kevice <kevice@mail.com>
2026-01-09 01:01:13 +0800

    move AI related codes to kudos-ai project

--- kudos-ability/kudos-ability-data/kudos-ability-data-vdb/kudos-ability-data-vdb-common/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-dependencies {
-    api(project(":kudos-context"))
-    api(libs.spring.ai.vector.store)
-
-    testImplementation(project(":kudos-test:kudos-test-common"))
-}
--- kudos-ability/kudos-ability-data/kudos-ability-data-vdb/kudos-ability-data-vdb-milvus/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-dependencies {
-    api(project(":kudos-ability:kudos-ability-data:kudos-ability-data-vdb:kudos-ability-data-vdb-common"))
-    api(libs.spring.ai.starter.vector.store.milvus)
-
-    testImplementation(libs.spring.ai.starter.model.ollama)
-    testImplementation(project(":kudos-test:kudos-test-container"))
-}
