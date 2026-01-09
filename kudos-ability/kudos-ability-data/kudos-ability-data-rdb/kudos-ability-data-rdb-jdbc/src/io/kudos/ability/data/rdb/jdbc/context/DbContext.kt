e26e584de0d653c6e2e74abd6ac9b0055e5de45c
kevice <kevice@mail.com>
2026-01-10 01:30:55 +0800

    clear ThreadLocal when work done

--- kudos-ability/kudos-ability-data/kudos-ability-data-rdb/kudos-ability-data-rdb-jdbc/src/io/kudos/ability/data/rdb/jdbc/context/DbContext.kt
+++ kudos-ability/kudos-ability-data/kudos-ability-data-rdb/kudos-ability-data-rdb-jdbc/src/io/kudos/ability/data/rdb/jdbc/context/DbContext.kt
@@ -1,5 +1,15 @@
 package io.kudos.ability.data.rdb.jdbc.context
 
+/**
+ * 数据库上下文
+ * 
+ * 使用 InheritableThreadLocal 存储线程上下文，支持父子线程间传递。
+ * 注意：在使用线程池的场景下，必须在请求/任务结束后调用 clear() 方法清理上下文，
+ * 避免内存泄漏。
+ *
+ * @author K
+ * @since 1.0.0
+ */
 class DbContext {
     private val contextParam: ThreadLocal<DbParam?> = object : InheritableThreadLocal<DbParam?>() {
         override fun childValue(parentValue: DbParam?): DbParam? {
@@ -11,15 +21,43 @@ class DbContext {
     companion object {
         private val self = DbContext()
 
+        /**
+         * 设置当前线程的数据库上下文参数
+         *
+         * @param param 数据库参数
+         * @author K
+         * @since 1.0.0
+         */
         fun set(param: DbParam?) {
             self.contextParam.set(param)
         }
 
+        /**
+         * 获取当前线程的数据库上下文参数
+         * 如果当前线程没有上下文，会创建一个新的并设置到 ThreadLocal 中
+         *
+         * @return 数据库参数
+         * @author K
+         * @since 1.0.0
+         */
         fun get(): DbParam {
             if (self.contextParam.get() == null) {
                 self.contextParam.set(DbParam())
             }
             return self.contextParam.get()!!
         }
+
+        /**
+         * 清除当前线程的数据库上下文
+         * 
+         * 建议在请求/任务结束时调用此方法，避免线程池复用线程时造成上下文污染和内存泄漏。
+         * 
+         * @author K
+         * @since 1.0.0
+         */
+        fun clear() {
+            self.contextParam.remove()
+        }
+
     }
 }
