67cb9b01e8ce97deacd3ede9cd8abb0817f874b2
kevice <kevice@mail.com>
2026-01-31 00:13:13 +0800

    feat: add module kudos-ams-msg

---
+++ kudos-ams/kudos-ams-msg/kudos-ams-msg-consumer/build.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+    api(project(":kudos-ams:kudos-ams-msg:kudos-ams-msg-common"))
+    api(project(":kudos-ability:kudos-ability-distributed:kudos-ability-distributed-client:kudos-ability-distributed-client-feign"))
+
+    //region your codes 1
+
+    //endregion your codes 1
+
+    testImplementation(project(":kudos-test:kudos-test-container"))
+}
