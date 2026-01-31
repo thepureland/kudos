0cc73d6a3ecc82ceacd1d98269ac68283ab034a7
Kevice <kevice@mail.com>
2026-02-01 01:01:08 +0800

    feat: support cache for UserAccountThird

--- kudos-ams/kudos-ams-user/kudos-ams-user-provider/resources/sql/user/h2/V1.0.0.3__insert_sys_cache.sql
+++ kudos-ams/kudos-ams-user/kudos-ams-user-provider/resources/sql/user/h2/V1.0.0.3__insert_sys_cache.sql
@@ -7,6 +7,8 @@ values ('a1a1a1a1-1111-1111-1111-111111111101', 'USER_ORG_BY_ID', 'ams-auth', 'L
         '用户缓存(by id)', true, true),
        ('a1a1a1a1-1111-1111-1111-111111111104', 'USER_ID_BY_TENANT_ID_AND_USERNAME', 'ams-auth', 'LOCAL_REMOTE', true, true, 999999999,
         '用户ID缓存(by tenantId & username)', true, true),
+       ('a1a1a1a1-1111-1111-1111-111111111105', 'ACCOUNT_THIRD_BY_USER_ID_AND_PROVIDER_CODE', 'ams-auth', 'LOCAL_REMOTE', true, true, 999999999,
+        '第三方账号缓存(by userId & providerCode)', true, true),
        ('a1a1a1a1-1111-1111-1111-111111111106', 'USER_ORG_IDS_BY_TENANT_ID', 'ams-auth', 'LOCAL_REMOTE', true, true, 999999999,
         '机构ID列表缓存(by tenantId)', true, true),
        ('a1a1a1a1-1111-1111-1111-111111111107', 'USER_IDS_BY_ORG_ID', 'ams-auth', 'LOCAL_REMOTE', true, true, 999999999,
