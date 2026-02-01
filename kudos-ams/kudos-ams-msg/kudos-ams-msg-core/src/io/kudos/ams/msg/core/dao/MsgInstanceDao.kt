26dedb73db7aeddd5f72e6ad795860902784a04a
kevice <kevice@mail.com>
2026-02-01 20:11:37 +0800

    refactor: rename module name under kudos-ams-msg

---
+++ kudos-ams/kudos-ams-msg/kudos-ams-msg-core/src/io/kudos/ams/msg/core/dao/MsgInstanceDao.kt
@@ -0,0 +1,25 @@
+package io.kudos.ams.msg.core.dao
+
+import io.kudos.ability.data.rdb.ktorm.support.BaseCrudDao
+import io.kudos.ams.msg.core.model.po.MsgInstance
+import io.kudos.ams.msg.core.model.table.MsgInstances
+import org.springframework.stereotype.Repository
+
+
+/**
+ * 消息实例数据访问对象
+ *
+ * @author K
+ * @author AI: Codex
+ * @since 1.0.0
+ */
+@Repository
+//region your codes 1
+open class MsgInstanceDao : BaseCrudDao<String, MsgInstance, MsgInstances>() {
+//endregion your codes 1
+
+    //region your codes 2
+
+    //endregion your codes 2
+
+}
---
+++ kudos-ams/kudos-ams-msg/kudos-ams-msg-core/src/io/kudos/ams/msg/core/service/iservice/IMsgInstanceService.kt
@@ -0,0 +1,22 @@
+package io.kudos.ams.msg.core.service.iservice
+
+import io.kudos.ams.msg.core.model.po.MsgInstance
+import io.kudos.base.support.iservice.IBaseCrudService
+
+
+/**
+ * 消息实例业务接口
+ *
+ * @author K
+ * @author AI: Codex
+ * @since 1.0.0
+ */
+//region your codes 1
+interface IMsgInstanceService : IBaseCrudService<String, MsgInstance> {
+//endregion your codes 1
+
+    //region your codes 2
+
+    //endregion your codes 2
+
+}
