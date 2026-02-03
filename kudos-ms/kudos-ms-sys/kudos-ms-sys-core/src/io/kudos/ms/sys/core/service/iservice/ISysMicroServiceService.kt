30cb37c6980873ff722c7e302876f5adc4c187a6
kevice <kevice@mail.com>
2026-02-03 21:58:36 +0800

    feat: add some methods for MicroServiceApi

--- kudos-ms/kudos-ms-sys/kudos-ms-sys-core/src/io/kudos/ms/sys/core/service/iservice/ISysMicroServiceService.kt
+++ kudos-ms/kudos-ms-sys/kudos-ms-sys-core/src/io/kudos/ms/sys/core/service/iservice/ISysMicroServiceService.kt
@@ -4,6 +4,7 @@ import io.kudos.ms.sys.common.vo.microservice.SysMicroServiceCacheItem
 import io.kudos.ms.sys.common.vo.microservice.SysMicroServiceRecord
 import io.kudos.ms.sys.core.model.po.SysMicroService
 import io.kudos.base.support.iservice.IBaseCrudService
+import io.kudos.ms.sys.common.vo.microservice.SysMicroServiceDetail
 
 
 /**
@@ -19,14 +20,51 @@ interface ISysMicroServiceService : IBaseCrudService<String, SysMicroService> {
     //region your codes 2
 
     /**
-     * 根据编码从缓存获取微服务信息
+     * 返回所有启用的微服务(包括原子服务，原子服务本质上也是微服务)
      *
-     * @param code 微服务编码
-     * @return SysMicroServiceCacheItem，找不到返回null
+     * @return List<微服务(包括原子服务)缓存对象>
+     * @author K
+     * @since 1.0.0
+     */
+    fun getAllActiveMicroService(): List<SysMicroServiceCacheItem>
+
+    /**
+     * 返回所有启用的微服务,不包括原子服务
+     *
+     * @return List<微服务(不包括原子服务)缓存对象>，不存在返回空列表
+     * @author K
+     * @since 1.0.0
+     */
+    fun getAllActiveMicroServiceExcludeAtomicService(): List<SysMicroServiceCacheItem>
+
+    /**
+     * 返回所有启用的原子服务(atomicService为true的微服务)
+     *
+     * @return List<原子服务(微服务)缓存对象>, 不存在返回空列表
+     * @author K
+     * @since 1.0.0
+     */
+    fun getAllActiveAtomicService(): List<SysMicroServiceCacheItem>
+
+    /**
+     * 返回指定编码的微/原子服务，可能是未启用的
+     *
+     * @param code 微/原子服务编码
+     * @return 微/原子服务缓存对象，不存在返回null
      * @author K
      * @since 1.0.0
      */
     fun getMicroServiceByCode(code: String): SysMicroServiceCacheItem?
+
+    /**
+     * 返回指定父编码下的所有启用的原子服务
+     *
+     * @param parentCode 父编码（微服务编码）
+     * @return List<SysMicroServiceRecord>，不存在返回空列表
+     * @author K
+     * @since 1.0.0
+     */
+    fun getAllActiveAtomicServiceByParentCode(parentCode: String): List<SysMicroServiceRecord>
 
     /**
      * 更新启用状态，并同步缓存
@@ -38,16 +76,6 @@ interface ISysMicroServiceService : IBaseCrudService<String, SysMicroService> {
      * @since 1.0.0
      */
     fun updateActive(code: String, active: Boolean): Boolean
-
-    /**
-     * 获取微服务下的原子服务列表
-     *
-     * @param microServiceCode 微服务编码
-     * @return 原子服务记录列表
-     * @author K
-     * @since 1.0.0
-     */
-    fun getAtomicServicesByMicroServiceCode(microServiceCode: String): List<SysMicroServiceRecord>
 
     //endregion your codes 2
 
