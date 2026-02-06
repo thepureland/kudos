2ebbfc0a5ef6358057120b9fa199e19884cb9144
Kevice <kevice@mail.com>
2026-02-06 17:39:14 +0800

    feat: upgrade spring-cloud-alibaba to 2025.1.00

--- kudos-ability/kudos-ability-distributed/kudos-ability-distributed-stream/kudos-ability-distributed-stream-common/build.gradle.kts
+++ kudos-ability/kudos-ability-distributed/kudos-ability-distributed-stream/kudos-ability-distributed-stream-common/build.gradle.kts
@@ -1,5 +1,9 @@
 dependencies {
-    api(project(":kudos-ability:kudos-ability-data:kudos-ability-data-rdb:kudos-ability-data-rdb-ktorm"))
+    api(project(":kudos-ability:kudos-ability-data:kudos-ability-data-rdb:kudos-ability-data-rdb-ktorm")) {
+        exclude(group = "com.fasterxml.jackson.module", module = "jackson-module-kotlin")
+    }
     api(project(":kudos-ability:kudos-ability-data:kudos-ability-data-rdb:kudos-ability-data-rdb-flyway"))
     api(libs.spring.cloud.stream)
+    // Spring Boot 4 使用 Jackson 3 (tools.jackson)，Spring Cloud Function 需要 Jackson 3 的 Kotlin 模块
+    api(libs.jackson.module.kotlin)
 }
