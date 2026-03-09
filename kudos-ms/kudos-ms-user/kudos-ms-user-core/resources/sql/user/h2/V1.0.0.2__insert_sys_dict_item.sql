94afbca83c15505ea99f167e8b3d8b7808797744
kevice <kevice@mail.com>
2026-03-10 01:01:10 +0800

    chore: remove id in sql file

--- kudos-ms/kudos-ms-user/kudos-ms-user-core/resources/sql/user/h2/V1.0.0.2__insert_sys_dict_item.sql
+++ kudos-ms/kudos-ms-user/kudos-ms-user-core/resources/sql/user/h2/V1.0.0.2__insert_sys_dict_item.sql
@@ -1,52 +1,52 @@
 --region DML
 
 -- account_type
-merge into "sys_dict_item" ("id", "dict_id", "item_code", "item_name", "order_num", "remark", "active", "built_in") values
-    ('e8ff3f9a-a57a-4183-acct-type00000001', '68139ed2-dict-user-acct-type00000000', '00', 'account_type.00', 1, '终端用户', true, true),
-    ('e8ff3f9a-a57a-4183-acct-type00000002', '68139ed2-dict-user-acct-type00000000', '11', 'account_type.11', 2, '租户管理员', true, true),
-    ('e8ff3f9a-a57a-4183-acct-type00000003', '68139ed2-dict-user-acct-type00000000', '12', 'account_type.12', 3, '租户成员', true, true),
-    ('e8ff3f9a-a57a-4183-acct-type00000004', '68139ed2-dict-user-acct-type00000000', '21', 'account_type.21', 4, '门户管理员', true, true),
-    ('e8ff3f9a-a57a-4183-acct-type00000005', '68139ed2-dict-user-acct-type00000000', '22', 'account_type.22', 5, '门户成员', true, true);
+insert into "sys_dict_item" ("dict_id", "item_code", "item_name", "order_num", "remark", "active", "built_in") values
+    ('68139ed2-dict-user-acct-type00000000', '00', 'account_type.00', 1, '终端用户', true, true),
+    ('68139ed2-dict-user-acct-type00000000', '11', 'account_type.11', 2, '租户管理员', true, true),
+    ('68139ed2-dict-user-acct-type00000000', '12', 'account_type.12', 3, '租户成员', true, true),
+    ('68139ed2-dict-user-acct-type00000000', '21', 'account_type.21', 4, '门户管理员', true, true),
+    ('68139ed2-dict-user-acct-type00000000', '22', 'account_type.22', 5, '门户成员', true, true);
 
 -- account_status
-merge into "sys_dict_item" ("id", "dict_id", "item_code", "item_name", "order_num", "remark", "active", "built_in") values
-    ('e8ff3f9a-a57a-4183-acct-status000001', '68139ed2-dict-user-acct-status000000', '00', 'account_status.00', 1, '已注销', true, true),
-    ('e8ff3f9a-a57a-4183-acct-status000002', '68139ed2-dict-user-acct-status000000', '10', 'account_status.10', 2, '正常', true, true),
-    ('e8ff3f9a-a57a-4183-acct-status000003', '68139ed2-dict-user-acct-status000000', '20', 'account_status.20', 3, '锁定', true, true),
-    ('e8ff3f9a-a57a-4183-acct-status000004', '68139ed2-dict-user-acct-status000000', '30', 'account_status.30', 4, '账号过期', true, true),
-    ('e8ff3f9a-a57a-4183-acct-status000005', '68139ed2-dict-user-acct-status000000', '40', 'account_status.40', 5, '凭证过期', true, true);
+insert into "sys_dict_item" ("dict_id", "item_code", "item_name", "order_num", "remark", "active", "built_in") values
+    ('68139ed2-dict-user-acct-status000000', '00', 'account_status.00', 1, '已注销', true, true),
+    ('68139ed2-dict-user-acct-status000000', '10', 'account_status.10', 2, '正常', true, true),
+    ('68139ed2-dict-user-acct-status000000', '20', 'account_status.20', 3, '锁定', true, true),
+    ('68139ed2-dict-user-acct-status000000', '30', 'account_status.30', 4, '账号过期', true, true),
+    ('68139ed2-dict-user-acct-status000000', '40', 'account_status.40', 5, '凭证过期', true, true);
 
 -- account_provider
-merge into "sys_dict_item" ("id", "dict_id", "item_code", "item_name", "order_num", "remark", "active", "built_in") values
-    ('e8ff3f9a-a57a-4183-item-provider0001', '68139ed2-dict-user-acct-provider0000', 'google', 'account_provider.google', 1, '谷歌', true, true),
-    ('e8ff3f9a-a57a-4183-item-provider0002', '68139ed2-dict-user-acct-provider0000', 'apple', 'account_provider.apple', 2, '苹果', true, true),
-    ('e8ff3f9a-a57a-4183-item-provider0003', '68139ed2-dict-user-acct-provider0000', 'wechat', 'account_provider.wechat', 3, '微信', true, true),
-    ('e8ff3f9a-a57a-4183-item-provider0004', '68139ed2-dict-user-acct-provider0000', 'github', 'account_provider.github', 4, 'github', true, true);
+insert into "sys_dict_item" ("dict_id", "item_code", "item_name", "order_num", "remark", "active", "built_in") values
+    ('68139ed2-dict-user-acct-provider0000', 'google', 'account_provider.google', 1, '谷歌', true, true),
+    ('68139ed2-dict-user-acct-provider0000', 'apple', 'account_provider.apple', 2, '苹果', true, true),
+    ('68139ed2-dict-user-acct-provider0000', 'wechat', 'account_provider.wechat', 3, '微信', true, true),
+    ('68139ed2-dict-user-acct-provider0000', 'github', 'account_provider.github', 4, 'github', true, true);
 
 -- org_type
-merge into "sys_dict_item" ("id", "dict_id", "item_code", "item_name", "order_num", "remark", "active", "built_in") values
-    ('e8ff3f9a-a57a-4183-0org-type00000001', '68139ed2-dict-user-0org-type00000000', '00', 'org_type.00', 1, '总部', true, false),
-    ('e8ff3f9a-a57a-4183-0org-type00000002', '68139ed2-dict-user-0org-type00000000', '10', 'org_type.10', 2, '分公司', true, false),
-    ('e8ff3f9a-a57a-4183-0org-type00000003', '68139ed2-dict-user-0org-type00000000', '20', 'org_type.20', 3, '事业部', true, false),
-    ('e8ff3f9a-a57a-4183-0org-type00000004', '68139ed2-dict-user-0org-type00000000', '30', 'org_type.30', 4, '部门', true, false),
-    ('e8ff3f9a-a57a-4183-0org-type00000005', '68139ed2-dict-user-0org-type00000000', '40', 'org_type.40', 5, '小组', true, false);
+insert into "sys_dict_item" ("dict_id", "item_code", "item_name", "order_num", "remark", "active", "built_in") values
+    ('68139ed2-dict-user-0org-type00000000', '00', 'org_type.00', 1, '总部', true, false),
+    ('68139ed2-dict-user-0org-type00000000', '10', 'org_type.10', 2, '分公司', true, false),
+    ('68139ed2-dict-user-0org-type00000000', '20', 'org_type.20', 3, '事业部', true, false),
+    ('68139ed2-dict-user-0org-type00000000', '30', 'org_type.30', 4, '部门', true, false),
+    ('68139ed2-dict-user-0org-type00000000', '40', 'org_type.40', 5, '小组', true, false);
 
 -- contact_way
-merge into "sys_dict_item" ("id", "dict_id", "item_code", "item_name", "order_num", "remark", "active", "built_in") values
-    ('e8ff3f9a-a57a-4183-item-contactway01', '68139ed2-dict-user-0000-contactway00', '101', 'contact_way.101', 1, '手机', true, true),
-    ('e8ff3f9a-a57a-4183-item-contactway02', '68139ed2-dict-user-0000-contactway00', '102', 'contact_way.102', 2, '固定电话', true, true),
-    ('e8ff3f9a-a57a-4183-item-contactway03', '68139ed2-dict-user-0000-contactway00', '201', 'contact_way.201', 3, 'email', true, true),
-    ('e8ff3f9a-a57a-4183-item-contactway04', '68139ed2-dict-user-0000-contactway00', '301', 'contact_way.301', 4, 'whatsapp', true, false),
-    ('e8ff3f9a-a57a-4183-item-contactway05', '68139ed2-dict-user-0000-contactway00', '302', 'contact_way.302', 5, 'wechat', true, false),
-    ('e8ff3f9a-a57a-4183-item-contactway06', '68139ed2-dict-user-0000-contactway00', '401', 'contact_way.401', 6, 'douyin', true, false),
-    ('e8ff3f9a-a57a-4183-item-contactway07', '68139ed2-dict-user-0000-contactway00', '402', 'contact_way.402', 7, 'tiktok', true, false),
-    ('e8ff3f9a-a57a-4183-item-contactway08', '68139ed2-dict-user-0000-contactway00', '403', 'contact_way.403', 8, 'facebook', true, false);
+insert into "sys_dict_item" ("dict_id", "item_code", "item_name", "order_num", "remark", "active", "built_in") values
+    ('68139ed2-dict-user-0000-contactway00', '101', 'contact_way.101', 1, '手机', true, true),
+    ('68139ed2-dict-user-0000-contactway00', '102', 'contact_way.102', 2, '固定电话', true, true),
+    ('68139ed2-dict-user-0000-contactway00', '201', 'contact_way.201', 3, 'email', true, true),
+    ('68139ed2-dict-user-0000-contactway00', '301', 'contact_way.301', 4, 'whatsapp', true, false),
+    ('68139ed2-dict-user-0000-contactway00', '302', 'contact_way.302', 5, 'wechat', true, false),
+    ('68139ed2-dict-user-0000-contactway00', '401', 'contact_way.401', 6, 'douyin', true, false),
+    ('68139ed2-dict-user-0000-contactway00', '402', 'contact_way.402', 7, 'tiktok', true, false),
+    ('68139ed2-dict-user-0000-contactway00', '403', 'contact_way.403', 8, 'facebook', true, false);
 
 -- contact_way_status
-merge into "sys_dict_item" ("id", "dict_id", "item_code", "item_name", "order_num", "remark", "active", "built_in") values
-    ('e8ff3f9a-a57a-4183-stas-contactway01', '68139ed2-dict-user-0000-contactwayst', '00', 'contact_way_status.00', 1, '未验证', true, true),
-    ('e8ff3f9a-a57a-4183-stas-contactway02', '68139ed2-dict-user-0000-contactwayst', '10', 'contact_way_status.10', 2, '正常', true, true),
-    ('e8ff3f9a-a57a-4183-stas-contactway03', '68139ed2-dict-user-0000-contactwayst', '20', 'contact_way_status.20', 3, '无法联系', true, true),
-    ('e8ff3f9a-a57a-4183-stas-contactway04', '68139ed2-dict-user-0000-contactwayst', '30', 'contact_way_status.30', 4, '非本人联系方式', true, true);
+insert into "sys_dict_item" ("dict_id", "item_code", "item_name", "order_num", "remark", "active", "built_in") values
+    ('68139ed2-dict-user-0000-contactwayst', '00', 'contact_way_status.00', 1, '未验证', true, true),
+    ('68139ed2-dict-user-0000-contactwayst', '10', 'contact_way_status.10', 2, '正常', true, true),
+    ('68139ed2-dict-user-0000-contactwayst', '20', 'contact_way_status.20', 3, '无法联系', true, true),
+    ('68139ed2-dict-user-0000-contactwayst', '30', 'contact_way_status.30', 4, '非本人联系方式', true, true);
 
 --endregion DML
--- kudos-ms/kudos-ms-user/kudos-ms-user-core/resources/sql/user/h2/V1.0.0.3__insert_sys_cache.sql
+++ kudos-ms/kudos-ms-user/kudos-ms-user-core/resources/sql/user/h2/V1.0.0.3__insert_sys_cache.sql
@@ -1,23 +1,13 @@
 --region DML
-insert into "sys_cache" ("id", "name", "atomic_service_code", "strategy_dict_code", "write_on_boot", "write_in_time",
-                         "ttl", "remark", "active", "built_in", "hash")
-values ('a1a1a1a1-1111-1111-1111-111111111102', 'USER_BY_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999,
-        '用户缓存(by id)', true, true, false),
-       ('a1a1a1a1-1111-1111-1111-111111111104', 'USER_ID_BY_TENANT_ID_AND_USERNAME', 'auth', 'LOCAL_REMOTE', true, true, 999999999,
-        '用户ID缓存(by tenantId & username)', true, true, false),
-       ('a1a1a1a1-1111-1111-1111-111111111105', 'ACCOUNT_THIRD_BY_USER_ID_AND_PROVIDER_CODE', 'auth', 'LOCAL_REMOTE', true, true, 999999999,
-        '第三方账号缓存(by userId & providerCode)', true, true, false),
-       ('a1a1a1a1-1111-1111-1111-111111111107', 'USER_IDS_BY_ORG_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999,
-        '用户ID列表缓存(by orgId)', true, true, false),
-       ('a1a1a1a1-1111-1111-1111-111111111108', 'USER_CONTACT_WAY_BY_USER_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999,
-        '用户联系方式缓存(by userId)', true, true, false),
-       ('a1a1a1a1-1111-1111-1111-111111111109', 'REMEMBER_ME_BY_TENANT_ID_AND_USERNAME', 'auth', 'LOCAL_REMOTE', true, true, 999999999,
-        '记住我登录缓存(by tenantId & username)', true, true, false),
-       ('a1a1a1a1-1111-1111-1111-111111111113', 'USER_ORG_IDS_BY_USER_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999,
-        '机构ID列表缓存(by userId)', true, true, false),
-       ('a1a1a1a1-1111-1111-1111-111111111120', 'USER_ORG__HASH', 'auth', 'LOCAL_REMOTE', true, true, 999999999,
-        '机构Hash缓存', true, true, true),
-       ('a1a1a1a1-1111-1111-1111-111111111121', 'USER_ACCOUNT__HASH', 'auth', 'LOCAL_REMOTE', true, true, 999999999,
-        '用户Hash缓存', true, true, true);
+insert into "sys_cache" ("name", "atomic_service_code", "strategy_dict_code", "write_on_boot", "write_in_time", "ttl", "remark", "hash") values 
+   ('USER_BY_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户缓存(by id)', false),
+   ('USER_ID_BY_TENANT_ID_AND_USERNAME', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户ID缓存(by tenantId & username)', false),
+   ('ACCOUNT_THIRD_BY_USER_ID_AND_PROVIDER_CODE', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '第三方账号缓存(by userId & providerCode)', false),
+   ('USER_IDS_BY_ORG_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户ID列表缓存(by orgId)', false),
+   ('USER_CONTACT_WAY_BY_USER_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户联系方式缓存(by userId)', false),
+   ('REMEMBER_ME_BY_TENANT_ID_AND_USERNAME', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '记住我登录缓存(by tenantId & username)', false),
+   ('USER_ORG_IDS_BY_USER_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '机构ID列表缓存(by userId)', false),
+   ('USER_ORG__HASH', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '机构Hash缓存', true),
+   ('USER_ACCOUNT__HASH', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户Hash缓存', true);
 
 --endregion DML
