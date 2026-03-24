6ecd427fcccc65cd8f5be035e864f3a49ae8cab4
kevice <kevice@mail.com>
2026-03-24 23:41:37 +0900

    feat(db): built_in in sys_cache init by true

--- kudos-ms/kudos-ms-auth/kudos-ms-auth-core/resources/sql/auth/h2/V1.0.0.3__insert_sys_cache.sql
+++ kudos-ms/kudos-ms-auth/kudos-ms-auth-core/resources/sql/auth/h2/V1.0.0.3__insert_sys_cache.sql
@@ -1,17 +1,17 @@
 --region DML
-insert into "sys_cache" ("name", "atomic_service_code", "strategy_dict_code", "write_on_boot", "write_in_time", "ttl", "remark", "hash") values
-    ('AUTH_USER_IDS_BY_TENANT_ID_AND_ROLE_CODE', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户ID列表缓存(by tenantId & roleCode)', false),
-    ('AUTH_USER_IDS_BY_TENANT_ID_AND_GROUP_CODE', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户ID列表缓存(by tenantId & groupCode)', false),
-    ('AUTH_RESOURCE_IDS_BY_TENANT_ID_AND_ROLE_CODE', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '资源ID列表缓存(by tenantId & roleCode)', false),
-    ('AUTH_RESOURCE_IDS_BY_TENANT_ID_AND_GROUP_CODE', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '资源ID列表缓存(by tenantId & groupCode)', false),
-    ('AUTH_RESOURCE_IDS_BY_TENANT_ID_AND_USERNAME', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '资源ID列表缓存(by tenantId & username)', false),
-    ('AUTH_RESOURCE_IDS_BY_USER_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '资源ID列表缓存(by userId)', false),
-    ('AUTH_ROLE_IDS_BY_USER_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '角色ID列表缓存(by userId)', false),
-    ('AUTH_GROUP_IDS_BY_USER_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户组ID列表缓存(by userId)', false),
-    ( 'AUTH_RESOURCE_IDS_BY_ROLE_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '资源ID列表缓存(by roleId)', false),
-    ( 'AUTH_USER_IDS_BY_ROLE_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户ID列表缓存(by roleId)', false),
-    ( 'AUTH_USER_IDS_BY_GROUP_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户ID列表缓存(by groupId)', false),
-    ('AUTH_GROUP__HASH', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户组Hash缓存', true),
-    ('AUTH_ROLE__HASH', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '角色Hash缓存', true);
+insert into "sys_cache" ("name", "atomic_service_code", "strategy_dict_code", "write_on_boot", "write_in_time", "ttl", "remark", "built_in", "hash") values
+    ('AUTH_USER_IDS_BY_TENANT_ID_AND_ROLE_CODE', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户ID列表缓存(by tenantId & roleCode)', true, false),
+    ('AUTH_USER_IDS_BY_TENANT_ID_AND_GROUP_CODE', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户ID列表缓存(by tenantId & groupCode)', true, false),
+    ('AUTH_RESOURCE_IDS_BY_TENANT_ID_AND_ROLE_CODE', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '资源ID列表缓存(by tenantId & roleCode)', true, false),
+    ('AUTH_RESOURCE_IDS_BY_TENANT_ID_AND_GROUP_CODE', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '资源ID列表缓存(by tenantId & groupCode)', true, false),
+    ('AUTH_RESOURCE_IDS_BY_TENANT_ID_AND_USERNAME', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '资源ID列表缓存(by tenantId & username)', true, false),
+    ('AUTH_RESOURCE_IDS_BY_USER_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '资源ID列表缓存(by userId)', true, false),
+    ('AUTH_ROLE_IDS_BY_USER_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '角色ID列表缓存(by userId)', true, false),
+    ('AUTH_GROUP_IDS_BY_USER_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户组ID列表缓存(by userId)', true, false),
+    ( 'AUTH_RESOURCE_IDS_BY_ROLE_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '资源ID列表缓存(by roleId)', true, false),
+    ( 'AUTH_USER_IDS_BY_ROLE_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户ID列表缓存(by roleId)', true, false),
+    ( 'AUTH_USER_IDS_BY_GROUP_ID', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户ID列表缓存(by groupId)', true, false),
+    ('AUTH_GROUP__HASH', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '用户组Hash缓存', true, true),
+    ('AUTH_ROLE__HASH', 'auth', 'LOCAL_REMOTE', true, true, 999999999, '角色Hash缓存', true, true);
 
 --endregion DML
