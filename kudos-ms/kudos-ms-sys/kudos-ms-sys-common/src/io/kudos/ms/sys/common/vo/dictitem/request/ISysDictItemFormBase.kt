41f655498256ef905d0feb46b6939d795b1e2c21
kevice <kevice@mail.com>
2026-04-01 01:16:48 +0900

    feat: add form validation rules for sysDictItem

--- kudos-ms/kudos-ms-sys/kudos-ms-sys-common/src/io/kudos/ms/sys/common/vo/dictitem/request/ISysDictItemFormBase.kt
+++ kudos-ms/kudos-ms-sys/kudos-ms-sys-common/src/io/kudos/ms/sys/common/vo/dictitem/request/ISysDictItemFormBase.kt
@@ -1,7 +1,10 @@
 package io.kudos.ms.sys.common.vo.dictitem.request
 
+import io.kudos.base.bean.validation.constraint.annotations.Matches
 import io.kudos.base.bean.validation.constraint.annotations.MaxLength
+import io.kudos.base.bean.validation.support.RegExpEnum
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Positive
 
 /**
  * 字典项表单基础字段（新建 / 更新共用）
@@ -13,16 +16,21 @@ interface ISysDictItemFormBase {
 
     /** 字典项代码 */
     @get:NotBlank
+    @get:MaxLength(64)
+    @get:Matches(RegExpEnum.VAR_NAME)
     val itemCode: String
 
     /** 字典项名称 */
     @get:NotBlank
+    @get:MaxLength(64)
     val itemName: String
 
     /** 字典id */
+    @get:NotBlank
     val dictId: String
 
     /** 字典项排序 */
+    @get:Positive
     val orderNum: Int?
 
     /** 父id */
